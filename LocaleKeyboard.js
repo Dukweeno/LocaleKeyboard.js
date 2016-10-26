f2da65700b7356224fa618221b763f76815de4bd
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-10-27 00:35:15 +0200

    JS Lib skeletton, good night ;D

---
+++ LocaleKeyboard.js
@@ -0,0 +1,30 @@
+/*
+ * The lib creator/compiler to make the special lib.
+ */
+
+ /* Know this script URL */
+ var scripts = document.getElementsByTagName('script');
+ var scriptDOM = scripts[scripts.length - 1];
+ var scriptUrl = scriptDOM.src;
+
+class LocaleKeyboard {
+  constructor(lang) {
+    /* Create the path */
+    var baseUrl = scriptUrl.substring(0, scriptUrl.lastIndexOf("/"));;
+    var subDir = "/lang";
+
+    /* Lang file */
+    var langPath = baseUrl + subDir + '/' + lang + ".lang";
+
+    /* Process lib */
+
+
+    /* Set all this into var */
+    this.localeLib = "";
+  }
+
+  getLib() {
+    /* Just return the modified lib */
+    return this.localeLib;
+  }
+}
