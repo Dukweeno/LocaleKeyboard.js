94d25930691e7b0a44f14891e4a82249c3f07515
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-10-27 12:05:20 +0200

    I think this is working, but i need to test, i'll test later. Modified the lib.

--- LocaleKeyboard.js
+++ LocaleKeyboard.js
@@ -7,24 +7,51 @@
  var scriptDOM = scripts[scripts.length - 1];
  var scriptUrl = scriptDOM.src;
 
+/* Function to request files */
+function getFile(sUrl) {
+   // Init request
+   var oReq = new XMLHttpRequest();
+
+   // Sending request
+   oReq.open("GET", sUrl, false);
+   oReq.send(null);
+
+   // Getting response
+   if (oReq.readyState == 4 && (oReq.status == 200 || oReq.status === 0)) {
+     return oReq.responseText;
+   } else {
+     return undefined;
+   }
+}
+
+/* Main class */
 class LocaleKeyboard {
   constructor(lang) {
     /* Create the path */
-    var baseUrl = scriptUrl.substring(0, scriptUrl.lastIndexOf("/"));;
-    var subDir = "/locales";
+    this.baseUrl = scriptUrl.substring(0, scriptUrl.lastIndexOf("/"));
 
-    /* Lang file */
-    var langPath = baseUrl + subDir + '/' + lang + ".lang";
-
-    /* Process lib */
-
+    /* Get files */
+    var langFile = getFile(baseUrl + "/locales/" + lang + ".lang");
+    var libPartOne = getFile(baseUrl + "/src/LocaleKeyboard-@1.cpp");
+    var libPartTwo = getFile(baseUrl + "/src/LocaleKeyboard-@2.cpp");
 
     /* Set all this into var */
-    this.localeLib = "";
+    this.libSource = libPartOne + langFile + libPartTwo;
+    this.libHeader = getFile(baseUrl + "/src/LocaleKeyboard.h");
   }
 
-  getLib() {
+  getSource() {
     /* Just return the modified lib */
-    return this.localeLib;
+    return this.libSource;
+  }
+
+  getHeader() {
+    /* Just return the header */
+    return this.libHeader;
+  }
+
+  listLocales() {
+    /* List all files *.lang in locales/ dir */
+    return;
   }
 }
